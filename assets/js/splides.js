cbfddb2174ef8f5eedb017c6e018a8e41ba7c188
Allan Roberto Da Silva Gomes <allanrs@grupopanvel.com.br>
2022-09-26 08:39:54 -0300

    first commit

---
+++ assets/js/splides.js
@@ -0,0 +1,159 @@
+/*
+const destraveSlider = new Splide('#destraveSlider', {
+	type: 'loop',
+	arrows: true,
+	autoplay: false,
+    pagination: false,
+    gap: 20,
+	perPage: 5,
+	perMove: 1,
+	breakpoints: {
+		450: {
+            perPage: 1,
+            gap: 0,
+        },
+        700: {
+            perPage: 2,
+        },
+        900: {
+            perPage: 3,
+        },
+        1200: {
+            perPage: 4,
+        },
+        
+	},
+}).mount()
+*/
+const categoriaSlider = new Splide('#categoriaSlider', {
+	type: 'loop',
+    arrows: true,
+    pagination: false,
+	perPage: 7,
+	perMove: 1,
+	breakpoints: {
+		500: {
+            perPage: 1,
+        },
+        600: {
+            perPage: 3,
+        },
+        700: {
+            perPage: 3,
+        },
+        900: {
+            perPage: 4,
+        },
+        1000: {
+            perPage: 5,
+        },
+        1200: {
+            perPage: 6,
+        },
+        
+	},
+}).mount()
+/*
+const loversSlider = new Splide('#loversSlider', {
+	type: 'loop',
+	arrows: true,
+	autoplay: false,
+    pagination: false,
+    gap: 20,
+	perPage: 5,
+	perMove: 1,
+	breakpoints: {
+		450: {
+            perPage: 1,
+            gap: 0,
+        },
+        700: {
+            perPage: 2,
+        },
+        900: {
+            perPage: 3,
+        },
+        1200: {
+            perPage: 4,
+        },
+        
+	},
+}).mount()
+*/
+const cuponsSlider = new Splide('#cuponsSlider', {
+	type: 'loop',
+	arrows: true,
+	autoplay: false,
+    pagination: false,
+    gap: 20,
+	perPage: 4,
+	perMove: 1,
+	breakpoints: {
+		600: {
+            perPage: 1,
+            gap: 0,
+        },
+        900: {
+            perPage: 2,
+        },
+        1200: {
+            perPage: 3,
+        }
+        
+	},
+}).mount()
+
+const queridinhosSlider = new Splide('#queridinhosSlider', {
+	type: 'loop',
+	arrows: true,
+	autoplay: false,
+    pagination: false,
+    gap: 20,
+	perPage: 5,
+	perMove: 1,
+	breakpoints: {
+		450: {
+            perPage: 1,
+            gap: 0,
+        },
+        700: {
+            perPage: 2,
+        },
+        900: {
+            perPage: 3,
+        },
+        1200: {
+            perPage: 4,
+        },
+        
+	},
+}).mount()
+
+
+const blogSlider = new Splide('#blogSlider', {
+	type: 'loop',
+	arrows: true,
+	autoplay: false,
+    pagination: false,
+    gap: 20,
+	perPage: 6,
+    perMove: 1,
+	breakpoints: {
+        350: {
+            perPage: 1,
+        },
+        530: {
+            perPage: 2,
+        },
+        700: {
+            perPage: 3,
+        },
+        900: {
+            perPage: 4,
+        },
+        1200: {
+            perPage: 5,
+        },
+        
+	},
+}).mount()
